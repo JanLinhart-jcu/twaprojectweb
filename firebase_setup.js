e7a718e339cf909a33233dbffb87576e788f7767
Honza <honza@seznam.cz>
2025-06-05 11:37:31 +0200

    firebase_setup.js update

--- firebase_setup.js
+++ firebase_setup.js
@@ -4,13 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  // Tvoje konfigurace z Firebase Console
   apiKey: "AIzaSyCdDo7vug0DyJ37IZG-S-SVZxDPnL22Ujg",
-  authDomain: "twa---project-web-1ff94.web.app",
+  authDomain: "twa---project-web-1ff94.firebaseapp.com",
+  databaseURL: "https://twa---project-web-1ff94-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "twa---project-web-1ff94",
-  storageBucket: "your-project.appspot.com",
+  storageBucket: "twa---project-web-1ff94.firebasestorage.app",
   messagingSenderId: "266224242618",
-  appId: "1:266224242618:web:a1ba523da310e9b4add783"
+  appId: "1:266224242618:web:a1ba523da310e9b4add783",
+  measurementId: "G-DTX3B2Y34N"
 };
 
 const app = initializeApp(firebaseConfig);
