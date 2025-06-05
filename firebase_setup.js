b65744b02978be46a340016ddf609d06839ba0b4
Honza <honza@seznam.cz>
2025-06-05 11:29:41 +0200

    firestore update

--- firebase_setup.js
+++ firebase_setup.js
@@ -5,14 +5,15 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   // Tvoje konfigurace z Firebase Console
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
+  apiKey: "AIzaSyCdDo7vug0DyJ37IZG-S-SVZxDPnL22Ujg",
+  authDomain: "twa---project-web-1ff94.web.app",
+  projectId: "twa---project-web-1ff94",
   storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "your-app-id"
+  messagingSenderId: "266224242618",
+  appId: "1:266224242618:web:a1ba523da310e9b4add783"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
